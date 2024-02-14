bd8a08d683b3e4a638f6cd710ea7ccaff6bc654f
Damien Mehala <damien.mehala@datadoghq.com>
2024-02-14 21:45:16 +0100

    fix: ci

--- test/integration-test/conftest.py
+++ test/integration-test/conftest.py
@@ -9,6 +9,18 @@ from pathlib import Path
 from datetime import datetime
 import pytest
 
+# Fix issue on CI
+# Source: <https://github.com/psf/requests/issues/4937>
+import socket
+from urllib3.connection import HTTPConnection
+
+HTTPConnection.default_socket_options = HTTPConnection.default_socket_options + [
+    (socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1),
+    (socket.SOL_TCP, socket.TCP_KEEPIDLE, 45),
+    (socket.SOL_TCP, socket.TCP_KEEPINTVL, 10),
+    (socket.SOL_TCP, socket.TCP_KEEPCNT, 6),
+]
+
 CWD = os.path.dirname(__file__)
 CWD_DOCKER_COMPOSE = CWD
 
@@ -123,9 +135,6 @@ class Agent:
         if token is None:
             token = str(uuid.uuid4())
 
-        print(
-            f"URL = http://{self.host}:{self.port}/test/session/start?test_session_token={token}"
-        )
         r = requests.get(
             f"http://{self.host}:{self.port}/test/session/start?test_session_token={token}"
         )
