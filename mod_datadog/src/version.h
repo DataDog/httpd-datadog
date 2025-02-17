80a7fd6bc99d900a3cebb25923536ef02ac83237
Damien Mehala <damien.mehala@datadoghq.com>
2025-02-17 15:52:23 +0100

    update version

--- mod_datadog/src/version.h
+++ mod_datadog/src/version.h
@@ -3,4 +3,4 @@
 #include <string_view>
 
 // The release version at or before this code revision, e.g. "v1.0.3".
-inline constexpr std::string_view mod_datadog_version = "1.0.3";
+inline constexpr std::string_view mod_datadog_version = "1.0.3-rum+e8ca11f1";
