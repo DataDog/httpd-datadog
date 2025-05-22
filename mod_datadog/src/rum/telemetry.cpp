fe592ef83c79b946b35658ded41d03799fa1a9fe
Pablo Martínez Bernardo <pablo.martinezbernardo@datadoghq.com>
2025-05-22 16:30:44 +0200

    Address review comments

--- mod_datadog/src/rum/telemetry.cpp
+++ mod_datadog/src/rum/telemetry.cpp
@@ -4,13 +4,10 @@ using namespace datadog::telemetry;
 
 namespace datadog::rum::telemetry {
 
-std::shared_ptr<Counter> injection_skipped(new Counter{"injection.skipped",
-                                                       "rum", true});
-std::shared_ptr<Counter> injection_succeed(new Counter{"injection.succeed",
-                                                       "rum", true});
-std::shared_ptr<Counter> injection_failed(new Counter{"injection.failed", "rum",
-                                                      true});
-std::shared_ptr<Counter> content_security_policy(new Counter{
-    "injection.content_security_policy", "rum", true});
+const Counter injection_skipped{"injection.skipped", "rum", true};
+const Counter injection_succeed{"injection.succeed", "rum", true};
+const Counter injection_failed{"injection.failed", "rum", true};
+const Counter content_security_policy{"injection.content_security_policy",
+                                      "rum", true};
 
 }  // namespace datadog::rum::telemetry
